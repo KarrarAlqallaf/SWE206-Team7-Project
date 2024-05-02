bd3147d586fa5c744c587b11bf53815f7990094b
omaralshehri <Alshehri2292@gmail.com>
2024-05-02 18:01:32 +0300

    added methods

--- Reservation.java
+++ Reservation.java
@@ -1,2 +1,72 @@
+import java.util.HashSet;
+import java.util.List;
 public class Reservation {
+    private Individual reserver;
+    private int rN;
+    private Space space;
+    private Duration duration;
+    private HashSet<Duration> durations;
+    private List<Individual> members;
+    private Restriction restriction;
+    private String reason;
+
+    public Individual getReserver() {
+        return reserver;
+    }
+
+    public void setReserver(Individual individual) {
+        this.reserver = individual;
+    }
+
+    public int getRN() {
+        return rN;
+    }
+
+    public void setRN(int rN) {
+        this.rN = rN;
+    }
+
+    public Space getSpace() {
+        return space;
+    }
+
+    public void setSpace(Space space) {
+        this.space = space;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
+
+    public HashSet<Duration> getDurations() {
+        return durations;
+    }
+
+    public void setDurations(HashSet<Duration> durations) {
+        this.durations = durations;
+    }
+
+    public List<Individual> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Individual> members) {
+        this.members = members;
+    }
+
+    public Restriction getRestriction() {
+        return restriction;
+    }
+
+    public void setRestriction(Restriction restriction) {
+        this.restriction = restriction;
+    }
+
+    public String getReason() {
+        return reason;
+    }
 }
