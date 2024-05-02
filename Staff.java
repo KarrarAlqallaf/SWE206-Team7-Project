50f632838ccfef1718120b750b0d11ea6b35e11f
A <karrar3035@gmail.com>
2024-05-02 18:07:30 +0300

    Adding Staff.java Class

---
+++ Staff.java
@@ -0,0 +1,5 @@
+public class Staff extends Individual {
+    public Staff(String name, String id, String email, Gender gender, String password) {
+        super(name, id, email, gender, password);
+    }
+}
