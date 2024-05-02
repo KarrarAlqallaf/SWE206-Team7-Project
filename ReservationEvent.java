b05868b6331c14fcb1a2ec8f865892656ab827e3
omaralshehri <Alshehri2292@gmail.com>
2024-05-02 18:13:01 +0300

    added methods into ReservationEvent Class

--- ReservationEvent.java
+++ ReservationEvent.java
@@ -1,2 +1,31 @@
+import java.util.HashSet;
+
 public class ReservationEvent {
+    private int membersGoal;
+    private Duration eDuration;
+    private HashSet<Duration> eDurations;
+
+    public int getMembersGoal() {
+        return membersGoal;
+    }
+
+    public void setMembersGoal(int membersGoal) {
+        this.membersGoal = membersGoal;
+    }
+
+    public Duration getEDuration() {
+        return eDuration;
+    }
+
+    public void setEDuration(Duration eDuration) {
+        this.eDuration = eDuration;
+    }
+
+    public HashSet<Duration> getEDurations() {
+        return eDurations;
+    }
+
+    public void setEDurations(HashSet<Duration> eDurations) {
+        this.eDurations = eDurations;
+    }
 }
